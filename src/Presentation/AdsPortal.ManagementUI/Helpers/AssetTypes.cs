800b5c04717aeaa733d790801a54a70e10692b6c
Adam Bajguz <adam.bajguz@gmail.com>
2020-12-04 17:47:06 +0100

    ManagementUI fixes

--- src/Presentation/AdsPortal.ManagementUI/Helpers/AssetTypes.cs
+++ src/Presentation/AdsPortal.ManagementUI/Helpers/AssetTypes.cs
@@ -5,6 +5,7 @@
         Image,
         Video,
         Stylesheet,
+        Fonts,
         Script,
         Unspecified
     }
--- src/Presentation/AdsPortal.ManagementUI/Helpers/MediaHelper.cs
+++ src/Presentation/AdsPortal.ManagementUI/Helpers/MediaHelper.cs
@@ -4,27 +4,9 @@
     using AdsPortal.Common;
     using Serilog;
 
-    /*
-     * When:
-     * <PropertyGroup>
-     *     <EnableDefaultContentItems>false</EnableDefaultContentItems>
-     * </PropertyGroup>
-     * and
-     * <ItemGroup>
-     *     <None Include="wwwroot\**" CopyToOutputDirectory="Always" />
-     * </ItemGroup>
-     * are added to .csproj the content of wwwroot is not dupliaced in wwwroot/_content
-     *
-     * But with <EnableDefaultContentItems>false</EnableDefaultContentItems> Blazor pages do not work.
-     * Instead of this use:
-     *
-     *  <PropertyGroup>
-     *      <StaticWebAssetBasePath Condition="$(StaticWebAssetBasePath) == ''">/</StaticWebAssetBasePath>
-     *  </PropertyGroup>
-     */
     public static class MediaHelper
     {
-        private const bool Use_ContentFolder = false;
+        private const bool Use_ContentFolder = true;
         private const bool GloballyDisabledVersioning = false;
 
         //TODO read from config
@@ -32,10 +14,14 @@
         public const string ImagesFolderName = "images/";
         public const string VideosFolderName = "videos/";
         public const string StylesheetsFolderName = "css/";
+        public const string FontsFolderName = "fonts/";
         public const string ScriptsFolderName = "js/";
 
-        private static readonly string AssemblyFullName = typeof(MediaHelper).Assembly.GetName().Name ?? throw new NullReferenceException($"{nameof(AssemblyFullName)} is null in {nameof(MediaHelper)}");
-        private static readonly string path = Use_ContentFolder ? $"/_content/{AssemblyFullName}/{AssetsFolderName}" : $"/{AssetsFolderName}";
+        private static readonly string AssemblyFullName = typeof(MediaHelper).Assembly.GetName().Name ??
+                                                              throw new NullReferenceException($"{nameof(AssemblyFullName)} is null in {nameof(MediaHelper)}");
+
+        private static readonly string WwwRootPath = Use_ContentFolder ? $"/_content/{AssemblyFullName}/" : $"/";
+        private static readonly string AssetsPath = $"{WwwRootPath}{AssetsFolderName}/";
 
         public static string AsAsset(string resource, AssetTypes type = AssetTypes.Unspecified, bool? overrideVersioning = null)
         {
@@ -43,16 +29,36 @@
             {
                 Log.Error("Invalid resource {Resource} supplied to {Operation}", resource, $"{nameof(MediaHelper)}.{nameof(AsAsset)}");
 
-                return path;
+                return AssetsPath;
             }
 
             (string folder, bool versioned) = ResolveAssetFolderFromType(type);
 
-            string tmp = path + folder + resource.Trim().TrimStart('/', '\\');
+            string tmp = AssetsPath + folder + resource.Trim().TrimStart('/', '\\');
 
             if (!GloballyDisabledVersioning)
             {
                 if (versioned || (overrideVersioning is bool b && b))
+                    return tmp + "?v=" + GlobalAppConfig.AppInfo.AppVersionTextPlain;
+            }
+
+            return tmp;
+        }
+
+        public static string AsRoot(string resource, bool versioned = false)
+        {
+            if (string.IsNullOrWhiteSpace(resource))
+            {
+                Log.Error("Invalid resource {Resource} supplied to {Operation}", resource, $"{nameof(MediaHelper)}.{nameof(AsRoot)}");
+
+                return WwwRootPath;
+            }
+
+            string tmp = WwwRootPath + resource.Trim().TrimStart('/', '\\');
+
+            if (!GloballyDisabledVersioning)
+            {
+                if (versioned)
                     return tmp + "?v=" + GlobalAppConfig.AppInfo.AppVersionTextPlain;
             }
 
@@ -66,6 +72,7 @@
                 AssetTypes.Image => (ImagesFolderName, false),
                 AssetTypes.Video => (VideosFolderName, false),
                 AssetTypes.Stylesheet => (StylesheetsFolderName, true),
+                AssetTypes.Fonts => (FontsFolderName, false),
                 AssetTypes.Script => (ScriptsFolderName, true),
                 _ => (string.Empty, false),
             };
