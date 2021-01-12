f533ccd3cfa37900d3e52c8b9c2e043929d98912
Adam Bajguz <adam.bajguz@gmail.com>
2021-01-13 00:05:32 +0100

    Added Test env + CLI drop db command for integration tests

---
+++ src/CLI/Core/AdsPortal.CLI.Application/Commands/Tools/Database/DropDatabaseCommand.cs
@@ -0,0 +1,80 @@
+﻿namespace AdsPortal.CLI.Application.Commands
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Typin;
+    using Typin.Attributes;
+    using Typin.Console;
+    using Typin.Utilities;
+
+    [Command("tools database drop", Description = "Drops database.")]
+    public class DropDatabaseCommand : ICommand
+    {
+        [CommandOption("conn-string", 'c', Description = "Connection string.", FallbackVariableName = "CONNECTION_STRING")]
+        public string ConnectionString { get; init; } = @"Data Source=.\SQLEXPRESS;Integrated Security=True;Database=master";
+
+        [CommandOption("name", 'n', Description = "Database name.", FallbackVariableName = "DB_NAME")]
+        public string DatabaseName { get; init; } = "AdsPortal_Test";
+
+        private ILogger _logger;
+
+        public DropDatabaseCommand(ILogger<DropDatabaseCommand> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            try
+            {
+                CancellationToken cancellationToken = console.GetCancellationToken();
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    bool dbExists = await CheckDatabaseExists(connection, cancellationToken);
+
+                    if (dbExists)
+                    {
+                        using (SqlCommand command = new SqlCommand($"ALTER DATABASE [{DatabaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [{DatabaseName}];", connection))
+                        {
+                            await connection.OpenAsync(cancellationToken);
+                            await command.ExecuteNonQueryAsync(cancellationToken);
+                            console.Output.WithForegroundColor(ConsoleColor.Green, (o) => o.WriteLine($"Database '{DatabaseName}' deleted successfully."));
+                        }
+                    }
+                    else
+                    {
+                        console.Error.WithForegroundColor(ConsoleColor.DarkYellow, (e) => e.WriteLine($"Database '{DatabaseName}' does not exist."));
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                _logger.LogError(ex, $"Failed to drop database '{DatabaseName}'.");
+
+                console.Error.WithForegroundColor(ConsoleColor.Red, (e) => e.WriteLine("Error occured. Details:"));
+                ExceptionFormatter.WriteException(console.Error, ex);
+            }
+        }
+
+        private async Task<bool> CheckDatabaseExists(SqlConnection connection, CancellationToken cancellationToken = default)
+        {
+            using (SqlCommand command = new SqlCommand($"SELECT database_id FROM sys.databases WHERE Name = '{DatabaseName}';", connection))
+            {
+                await connection.OpenAsync(cancellationToken);
+                object? result = await command.ExecuteScalarAsync(cancellationToken);
+
+                if (result is not null && int.TryParse(result.ToString(), out int databaseID))
+                {
+                    return (databaseID > 0);
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ src/CLI/Core/AdsPortal.CLI.Application/Commands/Tools/Test/TestWebApiCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace AdsPortal.CLI.Application.Commands.Tools.Test
+{
+    using System.Threading.Tasks;
+    using Typin;
+    using Typin.Attributes;
+    using Typin.Console;
+
+    [Command("tools test WebApi", Description = "Tests 'AdsPortal.WebApi'.")]
+    public class TestWebApiCommand : ICommand
+    {
+        [CommandOption("conn-string", 'c', Description = "Connection string.", FallbackVariableName = "CONNECTION_STRING")]
+        public string ConnectionString { get; init; } = @"Data Source=.\SQLEXPRESS;Integrated Security=True;Database=master";
+
+        [CommandOption("name", 'n', Description = "Database name.", FallbackVariableName = "DB_NAME")]
+        public string DatabaseName { get; init; } = "AdsPortal_Test";
+
+        private readonly ICliApplicationLifetime _cliApplicationLifetime;
+        private readonly ICliCommandExecutor _cliCommandExecutor;
+
+        public TestWebApiCommand(ICliApplicationLifetime cliApplicationLifetime, ICliCommandExecutor cliCommandExecutor)
+        {
+            _cliApplicationLifetime = cliApplicationLifetime;
+            _cliCommandExecutor = cliCommandExecutor;
+        }
+
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            await _cliCommandExecutor.ExecuteCommandAsync(@$"tools database drop -c ""{ConnectionString}"" -n ""{DatabaseName}""");
+
+            _cliApplicationLifetime.RequestStop();
+        }
+    }
+}
